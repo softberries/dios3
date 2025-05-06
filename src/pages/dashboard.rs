5df52f5b426c4fa2b8aff442b677c1be9417e55d
Kris <krzysztof.grajek@googlemail.com>
2025-05-06 15:59:06 +0200

    change github project links

--- src/pages/dashboard.rs
+++ src/pages/dashboard.rs
@@ -26,7 +26,7 @@ fn GithubStarAction() -> Element {
     rsx!(
         a {
     class: "flex items-center justify-between p-4 mb-8 text-sm font-semibold text-purple-100 bg-purple-600 rounded-lg shadow-md focus:outline-none focus:shadow-outline-purple",
-    href: "https://github.com/estevanmaito/windmill-dashboard",
+    href: "https://github.com/softberries/dios3",
     target: "_blank", // optional, for opening in new tab
     rel: "noopener noreferrer", // optional, for security
 
